4503995085de51711c4ef5c3eecaec3e11283eb7
kmisik <38467359+KaterinMisik@users.noreply.github.com>
2020-08-27 14:35:01 +0200

    Create qimagewidget.h

---
+++ qimagewidget.h
@@ -0,0 +1,155 @@
+#ifndef QIMAGEWIDGET_H
+#define QIMAGEWIDGET_H
+
+#include <QWidget>
+#include <QPixmap>
+#include <QMargins>
+#include <QPoint>
+
+#include <QMouseEvent>
+#include "recttodraw.h"
+#include "toolbar.h"
+#include <QLineF>
+
+#include "triangletodraw.h"
+
+
+
+class QImageWidget : public QWidget
+{
+    Q_OBJECT
+    ToolBar *toolBar;
+    QRectF creatorRect, creatorRectangle, new_creatorRectangle;
+    QList<RectToDraw> rects, circles;
+    TriangleToDraw creatorTriangles;
+    QLineF creatorTriangle;
+    QPoint endPos;
+    QPoint creatorTrial, new_creatorTriangle;
+
+public:
+    explicit QImageWidget(ToolBar *toolB, QWidget *parent = nullptr);
+    ~QImageWidget();
+    explicit QImageWidget(const QPoint &startingPos) :
+        new_creatorRectangle(startingPos, startingPos),
+        currentTriangle()
+    {
+        setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
+        setAttribute(Qt::WA_TranslucentBackground);
+        showMaximized();
+    }
+private:
+    QString _noFigureMessage;
+    QPixmap _originalImage;
+    QString _noImageMessage;
+    QString new_data;
+    int _margin;
+    QList<TriangleToDraw> triangles;
+
+    QList<QPoint> pointsList;
+    QList<QPointF> pointsList1;
+    QMargins _margins;
+    int _point;
+    QPoint _points;
+    QColor _backgroundColor;
+    QColor _Color;
+    QString _Tomany;
+    QPointF previousPoint;
+    TriangleToDraw currentTriangle, currentTriangle1;
+
+    QRect actualImageRect();
+    void updateMargins();
+    void updatePoints();
+public:
+    void setPixmap(QPixmap pixmap);
+    QPixmap pixmap(){
+        return _originalImage;
+    }
+
+    int margin(){return _margin;}
+    int point(){return _point;}
+
+public slots:
+
+  // void ToolBar::UpdatePoints();
+   void UploadPointsToConfig();
+ //  void UploadPointsToConfig1();
+signals:
+    void pointChanged();
+    void marginChanged();
+
+protected:
+
+    virtual void mouseDoubleClickEvent(QMouseEvent *event) override;
+
+public:
+    QPointF startPoint() const;
+    QPointF endPoint() const;
+    QLineF angleline, newLine, newLine1, newLine2, newLine3;
+    QLineF angleline2;
+    QRect m_currentRegion;
+    void setStartPoint(const QPointF point);
+    void setEndPoint(const QPointF point);
+    void SaveFile(QString filepath);
+    void SaveFile2(QString filepath2);
+    void SaveFile3(QString filepath3);
+    void SaveFile4(QString filepath4);
+    void emitSlot();
+private slots:
+    void UploadPoints(QString filepathtoupload);
+  //  void UploadPoints1(QString filepathtoupload);
+signals:
+    void regionSelected(const QRect &);
+private:
+    QRect currentRegion;
+
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+
+private:
+    void drawRectTo(const QPoint &endPoint);
+
+    int Triangling = 0;
+
+    bool modified = false;
+    bool Rectangling = false;
+    bool RectanglingforCircle = false;
+    bool Circling = false;
+    bool saveboxcontrol = false;
+    bool uploadcontrol = false;
+    int myPenWidth = 1;
+    QColor myPenColor = Qt::blue;
+    QImage image;
+    QPoint lastPoint, PPoint;
+    void drawRect(const QPoint &endPoint);
+    void drawRectangle(const QPoint &endPoint);
+    void drawEllipse(const QPoint &creatorRect);
+    void drawTriangle(const QPoint &endPoint);
+    void drawLine(const QPoint &endPoint);
+   // void drawLine(const QPointF endPoint);
+  //  void drawLines(QLineF, QLineF, QLineF);
+    void drawLines(const QVector<QLineF> &);
+    void drawTriangl(QLineF, QLineF, QLineF);
+
+    void setTopRight(const QPointF lastPoint);
+    void setTopLeft(const QPointF endPoint);
+    void setTopLeft0(const QPointF endPoint);
+    void setTopLeft1(const QPointF endPoint);
+    void setPoint (const QPointF );
+    void setTopRight1(const QPointF lastPoint);
+    void setPoints(const QPointF);
+    void setPoints1(const QPointF);
+    void setPoints2(const QPointF);
+    void setLine(const QLineF);
+
+//    QPoint newPoint1, newPoint2, newPoint3;
+
+};
+
+
+#endif // QIMAGEWIDGET_H
